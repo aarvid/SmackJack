d8db3093036f90dd5ed2f5e1f65f9fd14eaed594
aarvid <andy.arvid@gmail.com>
2013-09-25 13:55:43 -0400

    fixed defpackage symbols

--- package.lisp
+++ package.lisp
@@ -7,24 +7,24 @@ to run the ajax framework. ")
   (:shadowing-import-from #:parenscript :switch)
   (:export
            ;; smackjack.lisp
-           :ajax-processor
-           :ajax-namespace
-           :ajax-functions-namespace-p
-           :ajax-function-prefix
-           :ht-simple-ajax-symbols-p
-           :json-args-p
-           :server-uri
-           :default-content-type
-           :reply-external-format
-           :ht-simple-ajax-processor
-           :defun-ajax
-           :create-ajax-dispatcher
-           :generate-prologue
+           #:ajax-processor
+           #:ajax-namespace
+           #:ajax-functions-namespace-p
+           #:ajax-function-prefix
+           #:ht-simple-ajax-symbols-p
+           #:json-args-p
+           #:server-uri
+           #:default-content-type
+           #:reply-external-format
+           #:ht-simple-ajax-processor
+           #:defun-ajax
+           #:create-ajax-dispatcher
+           #:generate-prologue
            ;; pusher.lisp
-           :ajax-pusher
-           :pusher-poll-namespace
-           :pusher-poll-function-name
-           :pusher-poll-interval
-           :pusher-pushes-per-poll
-           :defun-push))
+           #:ajax-pusher
+           #:pusher-poll-namespace
+           #:pusher-poll-function-name
+           #:pusher-poll-interval
+           #:pusher-pushes-per-poll
+           #:defun-push))
 
