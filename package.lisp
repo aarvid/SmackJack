317636d104e07982d58df7e104243c7b3dcb84f1
aarvid <andy.arvid@gmail.com>
2013-09-25 14:17:12 -0400

    another package fix

--- package.lisp
+++ package.lisp
@@ -1,4 +1,5 @@
 ;;;; package.lisp
+(in-package :cl-user)
 
 (defpackage #:smackjack
   (:documentation "the one and only smackjack package. Contains all code
@@ -6,25 +7,25 @@ to run the ajax framework. ")
   (:use #:cl #:hunchentoot #:parenscript #:alexandria)
   (:shadowing-import-from #:parenscript :switch)
   (:export
-           ;; smackjack.lisp
-           #:ajax-processor
-           #:ajax-namespace
-           #:ajax-functions-namespace-p
-           #:ajax-function-prefix
-           #:ht-simple-ajax-symbols-p
-           #:json-args-p
-           #:server-uri
-           #:default-content-type
-           #:reply-external-format
-           #:ht-simple-ajax-processor
-           #:defun-ajax
-           #:create-ajax-dispatcher
-           #:generate-prologue
-           ;; pusher.lisp
-           #:ajax-pusher
-           #:pusher-poll-namespace
-           #:pusher-poll-function-name
-           #:pusher-poll-interval
-           #:pusher-pushes-per-poll
-           #:defun-push))
+   ;; smackjack.lisp
+   #:ajax-processor
+   #:ajax-namespace
+   #:ajax-functions-namespace-p
+   #:ajax-function-prefix
+   #:ht-simple-ajax-symbols-p
+   #:json-args-p
+   #:server-uri
+   #:default-content-type
+   #:reply-external-format
+   #:ht-simple-ajax-processor
+   #:defun-ajax
+   #:create-ajax-dispatcher
+   #:generate-prologue
+   ;; pusher.lisp
+   #:ajax-pusher
+   #:pusher-poll-namespace
+   #:pusher-poll-function-name
+   #:pusher-poll-interval
+   #:pusher-pushes-per-poll
+   #:defun-push))
 
